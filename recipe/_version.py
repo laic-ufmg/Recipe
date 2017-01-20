8f16e73575f5ad5f4a56dabd0211fa34dde1ac7c
walterjgsp <walterjgsp@gmail.com>
2017-01-20 08:54:54 -0200

    Setup.py implemented

---
+++ recipe/_version.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright 2016 Walter José
+
+This file is part of the RECIPE Algorithm.
+
+The RECIPE is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+RECIPE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. See http://www.gnu.org/licenses/.
+
+"""
+
+__version__ = '0.1'
