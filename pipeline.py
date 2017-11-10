d7e5818677607ceed64fcfc70660f4eeefcbb66d
Unknown <walterjgsp@gmail.com>
2017-11-10 17:14:33 -0200

    Grammar fix
    Fix grammar removing ensemble trees from the pos processing strategie.

--- pipeline.py
+++ pipeline.py
@@ -8,9 +8,9 @@ import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import Imputer
-from sklearn.feature_selection import VarianceThreshold
-from sklearn.svm import NuSVC, SVC
-from sklearn.tree import DecisionTreeClassifier, ExtraTreeClassifier
+from sklearn.preprocessing import StandardScaler, RobustScaler
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.neighbors import RadiusNeighborsClassifier
 None
 
 def pipeline(dataTraining,dataTest):
@@ -37,23 +37,22 @@ def pipeline(dataTraining,dataTest):
 	#Validation -- Get a subsample of the training to get information about possible overfitting:
 	X_train, X_validation, y_train, y_validation = train_test_split(train_data, train_target, train_size=0.7, test_size=0.3, random_state=dataSeed, stratify=train_target)
 
-	step0 = Imputer(axis=0, copy=True, missing_values='NaN', strategy='median', verbose=0)
+	step0 = Imputer(axis=0, copy=True, missing_values='NaN', strategy='most_frequent',
+    verbose=0)
 
-	step1 = VarianceThreshold(threshold=0.0)
+	step1 = StandardScaler(copy=True, with_mean='False', with_std=True)
 
 	step2 = FeatureUnion(n_jobs=1,
-       transformer_list=[('votingclassifier', VotingClassifier(estimators=[('alg0', SVC(C=1.0, cache_size=200, class_weight='balanced', coef0=0.0,
-  decision_function_shape=None, degree=2, gamma='auto', kernel='poly',
-  max_iter=2500, probability=False, random_state=42, shrinking=True,
-  tol=0.096956, verb...d91c80>, inv_kw_args=None,
+       transformer_list=[('votingclassifier', VotingClassifier(estimators=[('alg0', GradientBoostingClassifier(criterion='friedman_mse', init=None,
+              learning_rate=0.033942, loss='deviance', max_depth=32581,
+              max_features=None, max_leaf_nodes=76264,
+              min_impurity_split...6ad050>, inv_kw_args=None,
           inverse_func=None, kw_args=None, pass_y=False, validate=True))],
        transformer_weights=None)
 
-	step3 = DecisionTreeClassifier(class_weight=None, criterion='gini', max_depth=None,
-            max_features=0.864283, max_leaf_nodes=None,
-            min_impurity_split=1e-07, min_samples_leaf=1,
-            min_samples_split=2, min_weight_fraction_leaf=0.006302,
-            presort=True, random_state=42, splitter='random')
+	step3 = RadiusNeighborsClassifier(algorithm='brute', leaf_size=71, metric='manhattan',
+             metric_params=None, outlier_label=100000, p=10,
+             radius=1.751848, weights='uniform')
 
 	methods = []
 	methods.append(step0)
